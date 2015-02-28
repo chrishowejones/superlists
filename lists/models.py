c9075978c5d03177dcbda19347e3574ac1a98488
Chris Howe-Jones <chris.howejones@gmail.com>
2015-02-28 12:44:48 +0000

    Added List to models and related each To-Do item to a List.

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+# List class
+class List(models.Model):
+    pass
+
+
 # Item class
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
